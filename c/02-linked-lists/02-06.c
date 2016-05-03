d10f5ece485242fa27a0a320d46d8e6965719fc5
Matt Harrison <hi@matt-harrison.com>
2016-05-03 22:23:30 +0100

    02-06

---
+++ c/02-linked-lists/02-06.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "LinkedList.h"
+
+
+LinkedListNode *DetectLoopStartNode(LinkedListNode *start) {
+
+    LinkedListNode *slow = start->next;
+    LinkedListNode *fast = start->next->next;
+
+    while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    while (start != slow) {
+        start = start->next;
+        slow = slow->next;
+    }
+
+    return start;
+}
+
+
+int main(int argc, char **argv) {
+
+    LinkedListNode *H = LinkedListNew( 8, NULL );
+    LinkedListNode *G = LinkedListNew( 7, H );
+    LinkedListNode *F = LinkedListNew( 6, G );
+    LinkedListNode *E = LinkedListNew( 5, F );
+    LinkedListNode *D = LinkedListNew( 4, E );
+    LinkedListNode *C = LinkedListNew( 3, D );
+    LinkedListNode *B = LinkedListNew( 2, C );
+    LinkedListNode *A = LinkedListNew( 1, B );
+
+    H->next = D;
+
+    LinkedListNode *loopNode = DetectLoopStartNode(A);
+
+    printf("Loop start value is %d\n", loopNode->val);
+
+    return 0;
+}
