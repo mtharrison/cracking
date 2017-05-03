761b2ba12cb5845a9c806a109533bd934cd1363e
Matt Harrison <hi@matt-harrison.com>
2017-05-03 22:22:15 +0700

    Question 6

--- go/01-arrays-and-strings/question_6_test.go
+++ go/01-arrays-and-strings/question_6_test.go
@@ -4,27 +4,26 @@ import "testing"
 
 func TestRotateNNMatrix(t *testing.T) {
 
-	m := NNMatrix{
-		{1, 2, 3, 4},
-		{5, 6, 7, 8},
-		{9, 10, 11, 12},
-		{13, 14, 15, 16},
-	}
-
-	mRotated := NNMatrix{
-		{13, 9, 5, 1},
-		{14, 10, 6, 2},
-		{15, 11, 7, 3},
-		{16, 12, 8, 4},
-	}
-
 	type testCase struct {
 		input       NNMatrix
 		expectation NNMatrix
 	}
 
 	cases := []testCase{
-		{m, mRotated},
+		{
+			NNMatrix{
+				{1, 2, 3, 4},
+				{5, 6, 7, 8},
+				{9, 10, 11, 12},
+				{13, 14, 15, 16},
+			},
+			NNMatrix{
+				{13, 9, 5, 1},
+				{14, 10, 6, 2},
+				{15, 11, 7, 3},
+				{16, 12, 8, 4},
+			},
+		},
 	}
 
 	for _, c := range cases {
