1598fd18945342cdfcf551dd4bc5abcb78fa7b45
Matt Harrison <hi@matt-harrison.com>
2017-05-19 12:23:55 +0700

    A correct paths for robot implementation

--- go/src/cracking/09-recursion-dynamic-programming/question_2_test.go
+++ go/src/cracking/09-recursion-dynamic-programming/question_2_test.go
@@ -23,3 +23,23 @@ func TestPathsForRobot(t *testing.T) {
 		}
 	}
 }
+
+func TestPathsForRobotWithObstructions(t *testing.T) {
+
+	type testCase struct {
+		x, y, n int
+	}
+
+	cases := []testCase{
+		{3, 3, 20},
+		{5, 5, 252},
+		{10, 5, 3003},
+	}
+
+	for _, c := range cases {
+		result := pathsForRobot(c.x, c.y, 0, 0)
+		if result != c.n {
+			t.Errorf("Error calling pathsForRobot(%d, %d). Expected %d but got %d", c.x, c.y, c.n, result)
+		}
+	}
+}
